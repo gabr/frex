4ef77dc696378bca5e931bceddda06cefdb06efa
Arkadiusz Gabrys <arkadiuszgabrys@gmail.com>
2017-11-06 21:39:56 +0100

    replaceInFile in progress

--- frex.go
+++ frex.go
@@ -7,6 +7,7 @@ import (
 )
 
 const (
+	fileBufferSize = 1048576 // 1MB
 	minArgumentsCount = 3
 	help = `Error: %s
 
@@ -27,9 +28,9 @@ file1.txt before change    >    file1.txt after change
 )
 
 type arguments struct {
-	regex   *regexp.Regexp
-	replace string
-	files   []string
+	regex      *regexp.Regexp
+	replace    string
+	filesPaths []string
 }
 
 // Parses and validates user cli arguments
@@ -54,18 +55,73 @@ func parseArgs(userArgs []string) (arguments, error) {
 	args.replace = userArgs[1]
 
 	for _, filePath := range userArgs[2:] {
+		// TODO: Remove duplicates
 		if _, err := os.Stat(filePath); os.IsNotExist(err) {
 			return args, fmt.Errorf("File not found: %q", filePath)
 		}
+		append(args.filePaths
 	}
 
 	return args, nil
 }
 
+func replaceInFile(regex *regexp.Regexp,
+                   replace string,
+                   filePath string,
+                   end chan bool)
+{
+	defer (func () { end <- true })()	
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error while opening file %q: %s",
+			filePath, err)
+		return
+	}
+	defer file.Close()
+
+	// windows new line: 0D 0A
+	// linux   new line: 0A
+	// mac     new line: 0D or 0A
+
+	buffer := make([]byte, 0, fileBufferSize)
+	fileOff := 0
+	for {
+		n, err := file.ReadAt(buffer, fileOff)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error while reading file %q: %s",
+				filePath, err)
+			return
+		}
+
+		fileOff += n
+
+		// TODO: 
+
+		if n < fileBufferSize {
+			if n == 0 { break }
+
+			// TODO: Check the rest of the buffer
+		}
+	}
+}
+
 func main() {
-	_, err := parseArgs(os.Args[1:])
+	args, err := parseArgs(os.Args[1:])
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
+	}
+
+	end := make(chan bool)
+	filesCount := len(args.filesPaths)
+
+	for _, filePath := range args.filesPaths {
+		go replaceInFile(args.regex, args.replace, filePath, end)
+	}
+
+	// wait for all routines to finish
+	for i := 0; i < filesCount; i++ {
+		<-end
 	}
 }
